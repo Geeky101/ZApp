c36e3f94f871628d3d04558887ecb734b6451b72
Justin Mutsito <justinmutsito@gmail.com>
2018-04-16 14:57:35 +0200

    finalized payments view added show elements by checkbox function to items

--- app/src/main/java/com/justinmutsito/zapp/ui/PaymentsActivity.java
+++ app/src/main/java/com/justinmutsito/zapp/ui/PaymentsActivity.java
@@ -7,44 +7,50 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.justinmutsito.zapp.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PaymentsActivity extends AppCompatActivity {
 
-    @BindView(R.id.textView)
-    TextView mTextView;
-    @BindView(R.id.txtAmount)
-    EditText mTxtAmount;
-    @BindView(R.id.btnPayment)
-    Button mBtnPayment;
-    @BindView(R.id.chkTithe)
-    CheckBox mChkTithe;
-    @BindView(R.id.chkOffering)
-    CheckBox mChkOffering;
-    @BindView(R.id.chkBuilding)
-    CheckBox mChkBuilding;
-    @BindView(R.id.chkOther)
-    CheckBox mChkOther;
-    @BindView(R.id.txtOther)
-    EditText mTxtOther;
-    @BindView(R.id.vTithe)
-    TextView mVTithe;
-    @BindView(R.id.txtTithe)
-    EditText mTxtTithe;
-    @BindView(R.id.vOffering)
-    TextView mVOffering;
-    @BindView(R.id.vBuilding)
-    TextView mVBuilding;
-    @BindView(R.id.vOther)
-    TextView mVOther;
-    @BindView(R.id.txtOffering)
-    EditText mTxtOffering;
-    @BindView(R.id.txtBuilding)
-    EditText mTxtBuilding;
+
+    @BindView(R.id.amountLabel)
+    TextView mAmountLabel;
+    @BindView(R.id.amountField)
+    EditText mAmountField;
+    @BindView(R.id.paymentButton)
+    Button mPaymentButton;
+    @BindView(R.id.titheCheckBox)
+    CheckBox mTitheCheckBox;
+    @BindView(R.id.offeringCheckBox)
+    CheckBox mOfferingCheckBox;
+    @BindView(R.id.buildingCheckBox)
+    CheckBox mBuildingCheckBox;
+    @BindView(R.id.otherCheckBox)
+    CheckBox mOtherCheckBox;
+    @BindView(R.id.otherField)
+    EditText mOtherField;
+    @BindView(R.id.titheLabel)
+    TextView mTitheLabel;
+    @BindView(R.id.titheField)
+    EditText mTitheField;
+    @BindView(R.id.offeringLabel)
+    TextView mOfferingLabel;
+    @BindView(R.id.buildingLabel)
+    TextView mBuildingLabel;
+    @BindView(R.id.otherLabel)
+    TextView mOtherLabel;
+    @BindView(R.id.offeringField)
+    EditText mOfferingField;
+    @BindView(R.id.buildingField)
+    EditText mBuildingField;
+    int itemCount=0;
+    Integer totalAmount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,62 +60,71 @@ public class PaymentsActivity extends AppCompatActivity {
 
     }
 
-    public void selectItem(View view) {
-        EditText txtTithe, txtOffering, txtBuilding, txtOther;
-        TextView vTithe, vOffering, vBuilding, vOther;
-
-        txtTithe = findViewById(R.id.txtTithe);
-        txtOffering = findViewById(R.id.txtOffering);
-        txtBuilding = findViewById(R.id.txtBuilding);
-        txtOther = findViewById(R.id.txtOther);
-
-        vTithe = findViewById(R.id.vTithe);
-        vOffering = findViewById(R.id.vOffering);
-        vBuilding = findViewById(R.id.vBuilding);
-        vOther = findViewById(R.id.vOther);
-
+    @OnClick({R.id.amountLabel, R.id.amountField, R.id.paymentButton, R.id.titheCheckBox, R.id.offeringCheckBox, R.id.buildingCheckBox, R.id.otherCheckBox})
+    public void onViewClicked(View view) {
         boolean checked = ((CheckBox) view).isChecked();
+
         switch (view.getId()) {
-            case R.id.chkTithe:
-                if (checked) {
-                    txtTithe.setVisibility(View.VISIBLE);
-                    vTithe.setVisibility(View.VISIBLE);
-                } else {
-                    txtTithe.setVisibility(View.GONE);
-                    vTithe.setVisibility(View.GONE);
+            case R.id.paymentButton:
+                totalAmount = (Integer.parseInt(mTitheField.getText().toString()) + Integer.parseInt(mOfferingField.getText().toString()) + Integer.parseInt(mBuildingField.getText().toString()));
+                if (totalAmount.toString().equals(mAmountField.getText().toString())){
+                    Toast.makeText(getApplicationContext(), "Amounts equal", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Amounts do not equal", Toast.LENGTH_SHORT).show();
                 }
                 break;
-
-            case R.id.chkOffering:
-                if (checked) {
-                    txtOffering.setVisibility(View.VISIBLE);
-                    vOffering.setVisibility(View.VISIBLE);
-                } else {
-                    txtOffering.setVisibility(View.GONE);
-                    vOffering.setVisibility(View.GONE);
+            case R.id.titheCheckBox:
+                if (checked){
+                    itemCount++;
+                    mTitheLabel.setVisibility(View.VISIBLE);
+                    mTitheField.setVisibility(View.VISIBLE);
+                    if (itemCount==1){
+                        mTitheField.setText(mAmountField.getText());
+                        mTitheField.setEnabled(false);
+                    }
+                }else{
+                    itemCount--;
+                    mTitheField.setEnabled(true);
                 }
                 break;
-
-            case R.id.chkBuilding:
-                if (checked) {
-                    txtBuilding.setVisibility(View.VISIBLE);
-                    vBuilding.setVisibility(View.VISIBLE);
-                } else {
-                    txtBuilding.setVisibility(View.GONE);
-                    vBuilding.setVisibility(View.GONE);
+            case R.id.offeringCheckBox:
+                if (checked){
+                    itemCount++;
+                    mOfferingLabel.setVisibility(View.VISIBLE);
+                    mOfferingField.setVisibility(View.VISIBLE);
+                    if (itemCount==1){
+                        mOfferingField.setText(mAmountField.getText());
+                        mOfferingField.setEnabled(false);
+                    }
+                }else{
+                    itemCount--;
+                    mOfferingField.setEnabled(true);
                 }
                 break;
-
-            case R.id.chkOther:
-                if (checked) {
-                    txtOther.setVisibility(View.VISIBLE);
-                    vOther.setVisibility(View.VISIBLE);
-                } else {
-                    txtOther.setVisibility(View.GONE);
-                    vOther.setVisibility(View.GONE);
+            case R.id.buildingCheckBox:
+                if (checked){
+                    itemCount++;
+                    mBuildingLabel.setVisibility(View.VISIBLE);
+                    mBuildingField.setVisibility(View.VISIBLE);
+                    if (itemCount==1){
+                        mBuildingField.setText(mAmountField.getText());
+                        mBuildingField.setEnabled(false);
+                    }
+                }else{
+                    itemCount--;
+                    mBuildingField.setEnabled(true);
                 }
                 break;
-
+            case R.id.otherCheckBox:
+                if (checked){
+                    itemCount++;
+                    mOtherLabel.setVisibility(View.VISIBLE);
+                    mOtherField.setVisibility(View.VISIBLE);
+                }else{
+                    itemCount--;
+                }
+                break;
         }
+
     }
 }
